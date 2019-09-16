38f72222d268cbd8a8bce458b814819500d84f45
ashrafkamarudin <ashrafkamarudin1995@gmail.com>
2019-09-16 18:01:05 +0800

    added list command

---
+++ commands/list.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: 'list',
+	description: 'Get the song that is playing.',
+	execute(message) {
+        let str = '';
+		const serverQueue = message.client.queue.get(message.guild.id);
+        //if (!serverQueue) return message.channel.send('There is nothing playing.');
+
+        for (i = 0; i < serverQueue.songs.length; i++)
+            str += `${i+1} => Title: ${serverQueue.songs[i].title} \n`;
+
+        //console.log(serverQueue);
+        
+		return message.channel.send(str);
+	},
+};
